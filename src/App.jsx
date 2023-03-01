fca40653fd2dc4903bf420f8ce4596d7e5130765
JhosselynGonzalezSanchez7 <l19350296@tuxtepec.tecnm.mx>
2023-03-01 09:22:14 -0600

    Este es un programa relacionado a la lista de hobbies

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  const nombre ="Jhosselyn Gonzalez Sanchez"
+  return (
+    <div>
+  <h3>Lista de hobbies de {nombre}</h3>
+  <ol>
+    <li>pintar</li>
+    <li>Dibujar</li>
+    <li>Ver peliculas y series</li>
+    <li>Escuchar musica</li>
+    <li>Pasar tiempo con mi pareja</li>
+    <li>Pasar  tiempo con mis amigos</li>
+    <li>Jugar con mi mascota</li>
+    <li>Pasar tiempo con mi familia</li>
+  </ol>
+  </div>
+  )
+}
+export default App
